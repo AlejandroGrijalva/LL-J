cf2e27625f2db97f2b2dcaa782c0563c33850f93
Luis Fimbres G <2016.lafg@gmail.com>
2026-03-11 11:24:51 -0600

    Blade2

--- Turizteca/routes/web.php
+++ Turizteca/routes/web.php
@@ -1,7 +1,17 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Login
+Route::view('/login', 'auth.login')->name('login');
+
+// Dashboard
+Route::prefix('dashboard')->group(function () {
+    Route::view('/', 'dashboard.overview')->name('dashboard');
+    Route::view('/restaurants', 'dashboard.restaurants')->name('dashboard.restaurants');
+    Route::view('/sponsorships', 'dashboard.sponsorships')->name('dashboard.sponsorships');
+    Route::view('/reviews', 'dashboard.reviews')->name('dashboard.reviews');
+    Route::view('/users', 'dashboard.users')->name('dashboard.users');
+    Route::view('/settings', 'dashboard.settings')->name('dashboard.settings');
 });
+
+Route::redirect('/', '/dashboard');
