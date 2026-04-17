51f39e5548286dcd941675578563a8ee1fa646dc
Luis Fimbres G <2016.lafg@gmail.com>
2026-04-17 12:35:44 -0600

    Restaurants API

--- Turizteca/app/Http/Controllers/Api/RRestaurantsAPIController.php
+++ Turizteca/app/Http/Controllers/Api/RRestaurantsAPIController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 
 class RRestaurantsAPIController extends Controller
 {
@@ -11,7 +13,11 @@ class RRestaurantsAPIController extends Controller
      */
     public function index()
     {
-        //
+        $restaurants = Restaurant::with('owner')->get();
+        return response()->json([
+            "data" => $restaurants,
+            "status" => "success"
+        ]);
     }
 
     /**
@@ -27,7 +33,25 @@ class RRestaurantsAPIController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'owner_id' => 'required|numeric',
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+            'cuisine_type' => 'nullable|string',
+            'average_price' => 'nullable|numeric',
+            'location_lat' => 'nullable|numeric',
+            'location_lng' => 'nullable|numeric',
+            'opening_hours_type' => 'nullable|string',
+            'opens_at' => 'nullable|string',
+            'closes_at' => 'nullable|string'
+        ]);
+
+        $restaurant = Restaurant::create($request->all());
+
+        return response()->json([
+            "data" => $restaurant,
+            "status" => "success"
+        ], 201);
     }
 
     /**
@@ -35,7 +59,19 @@ class RRestaurantsAPIController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $restaurant = Restaurant::with('owner')->find($id);
+        
+        if ($restaurant == null) {
+            return response()->json([
+                "message" => "Restaurante no encontrado",
+                "status" => "error"
+            ], 404);
+        }
+
+        return response()->json([
+            "data" => $restaurant,
+            "status" => "success"
+        ]);
     }
 
     /**
@@ -51,7 +87,21 @@ class RRestaurantsAPIController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $restaurant = Restaurant::find($id);
+        
+        if ($restaurant == null) {
+            return response()->json([
+                "message" => "Restaurante no encontrado",
+                "status" => "error"
+            ], 404);
+        }
+
+        $restaurant->update($request->all());
+
+        return response()->json([
+            "data" => $restaurant,
+            "status" => "success"
+        ]);
     }
 
     /**
@@ -59,6 +109,20 @@ class RRestaurantsAPIController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $restaurant = Restaurant::find($id);
+        
+        if ($restaurant == null) {
+            return response()->json([
+                "message" => "Restaurante no encontrado",
+                "status" => "error"
+            ], 404);
+        }
+
+        $restaurant->delete();
+
+        return response()->json([
+            "message" => "Restaurante eliminado",
+            "status" => "success"
+        ], 204);
     }
 }
